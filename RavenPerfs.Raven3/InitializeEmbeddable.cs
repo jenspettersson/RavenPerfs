6a5e085f8c4d9ba19a5f1db6f2d795c86f25c944
Jens Pettersson <jens.pettersson@live.se>
2014-12-11 14:48:07 +0100

    Initial commit with some perf tests

---
+++ RavenPerfs.Raven3/InitializeEmbeddable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using Raven.Client.Embedded;
+using Raven.Client.Indexes;
+using RavenPerfs.Raven3.RavenTests;
+using Xunit;
+
+namespace RavenPerfs.Raven3
+{
+    public class InitializeEmbeddable
+    {
+        [Fact]
+        public void Initialize_without_index()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var embeddableDocumentStore = new EmbeddableDocumentStore
+            {
+                RunInMemory = true
+            };
+
+            embeddableDocumentStore.Initialize();
+            embeddableDocumentStore.Dispose();
+            Console.WriteLine("Done: {0}", stopwatch.Elapsed);
+        }
+
+        [Fact]
+        public void Initialize_with_index()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var embeddableDocumentStore = new EmbeddableDocumentStore
+            {
+                RunInMemory = true
+            };
+
+            embeddableDocumentStore.Initialize();
+            
+            IndexCreation.CreateIndexes(typeof(CanCallLastOnArray).Assembly, embeddableDocumentStore);
+
+            embeddableDocumentStore.Dispose();
+            Console.WriteLine("Done: {0}", stopwatch.Elapsed);
+        }
+
+        [Fact]
+        public void Initialize_without_index_100_times()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 100; i++)
+            {
+                var embeddableDocumentStore = new EmbeddableDocumentStore
+                {
+                    RunInMemory = true
+                };
+
+                embeddableDocumentStore.Initialize();
+                embeddableDocumentStore.Dispose();
+            }
+            Console.WriteLine("Done: {0}", stopwatch.Elapsed);
+        }
+    }
+}
