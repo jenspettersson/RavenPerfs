93eea63522fb8734d6f928f9f8d073e7e771a329
Jens Pettersson <jens.pettersson@live.se>
2015-10-07 14:23:53 +0200

    Added tests that verifies possible bug while loading documents with a constructor that defaults some values

---
+++ InitializationPerfs/RavenPerfs.Raven2_5/LoadCausesChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Raven.Client.Embedded;
+using Xunit;
+
+namespace RavenPerfs.Raven2_5
+{
+    public class Entity
+    {
+        public string Id { get; set; }
+        public List<string> History { get; set; }
+
+        public Entity()
+        {
+            History = new List<string> { "Entity created" };
+        }
+    }
+
+    public class LoadCausesChange
+    {
+        [Fact]
+        public void Load_on_state_with_list_in_ctor()
+        {
+            var store = new EmbeddableDocumentStore
+            {
+                RunInMemory = true
+            };
+            store.Initialize();
+
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Entity {Id = "Entities/1"});
+                session.SaveChanges();
+            }
+
+            using (var session = store.OpenSession())
+            {
+                var entity = session.Load<Entity>("Entities/1");
+
+                Assert.False(session.Advanced.HasChanges);
+            }
+        }
+    }
+}
--- InitializationPerfs/RavenPerfs.Raven3/InitializeEmbeddable.cs
+++ InitializationPerfs/RavenPerfs.Raven3/InitializeEmbeddable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Raven.Client.Embedded;
 using Raven.Client.Indexes;
